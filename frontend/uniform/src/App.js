dd50775ca03fa3625c65813fffe159c5ccd667b3
박주연 <ekscn09@naver.com>
2024-07-19 17:03:24 +0900

    feat(S11P12A308-216) : 메인 페이지 css 수정

--- frontend/uniform/src/App.js
+++ frontend/uniform/src/App.js
@@ -12,16 +12,18 @@ import NavBar from './components/NavBar';
 import Header from './components/Header';
 import styled from 'styled-components';
 
-const Content = styled.div`
-  margin-top: 60px;
+const AppContainer = styled.div`
+  background-color: #F8F8F8;
+  height: 100vh; /* 화면 전체 높이 */
+
+  /* flex-shrink: 0; */
 `
 function App() {
   return (
     <>
       <BrowserRouter>
-        <div className="App">
+        <AppContainer>
             <Header />
-            <Content>
             <Routes>
               <Route path="/" element={ <MainView /> }></Route>
               <Route path="/photo-card" element={ <PhotoCardView /> }></Route>
@@ -29,9 +31,8 @@ function App() {
               <Route path="/community" element={ <CommunityView /> }></Route>
               <Route path="/my-page" element={ <MyPageView /> }></Route>
             </Routes>
-            </Content>
             <NavBar />
-        </div>
+        </AppContainer>
       </BrowserRouter>
     </>
   );
