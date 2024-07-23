25bd8e3013460d4f6c6bab4535a5776cc9b8fab9
최예헌 <1594cyh@gmail.com>
2024-07-23 17:13:54 +0900

    feat(S11P12A308-362-be-diary-entity): diary entity

---
+++ backend/youniform/src/main/java/com/youniform/api/domain/diary/entity/Diary.java
@@ -0,0 +1,30 @@
+package com.youniform.api.domain.diary.entity;
+
+import com.youniform.api.domain.user.entity.Users;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Diary {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "diary_id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private Users userId;
+
+    private LocalDate diaryDate;
+
+    private Scope scope;
+}
