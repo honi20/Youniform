ca91dd7a0fbf82bb566159ad85ff66263f56808c
고민혁 <rhalsgur0086@gmail.com>
2024-07-23 16:35:24 +0900

    feat(S11P12A308-319-be-user-entity): User Entity 추가

--- backend/youniform/src/main/java/com/youniform/api/domain/team/entity/Team.java
+++ backend/youniform/src/main/java/com/youniform/api/domain/team/entity/Team.java
@@ -1,13 +1,18 @@
 package com.youniform.api.domain.team.entity;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import org.springframework.data.annotation.Id;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
-import java.util.Date;
 
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class Team {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
