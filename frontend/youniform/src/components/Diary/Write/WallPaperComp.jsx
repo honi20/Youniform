28c8a1bffd15bc6e90acef4b8f36e4988fd1b99d
박주연 <ekscn09@naver.com>
2024-08-02 17:42:33 +0900

    feat(S11P12A308-416-fe-diary-entry-add-color-chips-krozv): color chip 추가

--- frontend/youniform/src/components/Diary/Write/WallPaperComp.jsx
+++ frontend/youniform/src/components/Diary/Write/WallPaperComp.jsx
@@ -1,25 +1,25 @@
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 
 const WallpaperContainer = styled.div`
   display: flex;
-  justify-content: flex-start;
-  align-items: center;
-  flex-wrap: wrap;
-  gap: 16px; /* 이미지 간의 간격 */
-  padding: 16px;
-  overflow-y: auto;
-  max-height: 100%;
-  margin-bottom: 5px;
+  height: calc(100% - 40px);
+  border: 1px solid red;
 `;
-
+const Wrapper = styled.div`
+  padding-left: 3%;
+  display: flex;
+  flex-flow: row wrap;
+  gap: 3.3%;
+  margin: 10px;
+  overflow-y: auto;
+`;
 const WallpaperImgContainer = styled.div`
-  flex: 0 0 30%; /* 3열 레이아웃 */
+  flex: 0 0 30%;
   aspect-ratio: 6/4.5;
-  background: #f0f0f0; 
-  overflow: hidden; 
+  overflow: hidden;
   border-radius: 0.625rem;
-  border: 1px solid #000;
+  border: 1px solid black;
 `;
 
 const WallpaperImg = styled.img`
@@ -31,14 +31,18 @@ const WallpaperImg = styled.img`
 const WallPaperComp = ({ wallpapers, onImageClick }) => {
   return (
     <WallpaperContainer>
-      {wallpapers.map((imageUrl, index) => (
-        <WallpaperImgContainer key={index}
-            onClickCapture={() => onImageClick(imageUrl)}>
+      <Wrapper>
+        {wallpapers.map((imageUrl, index) => (
+          <WallpaperImgContainer
+            key={index}
+            onClickCapture={() => onImageClick(imageUrl)}
+          >
             <WallpaperImg src={imageUrl} alt={`Wallpaper ${index + 1}`} />
-        </WallpaperImgContainer>
-      ))}
+          </WallpaperImgContainer>
+        ))}
+      </Wrapper>
     </WallpaperContainer>
   );
-}
+};
 
 export default WallPaperComp;
