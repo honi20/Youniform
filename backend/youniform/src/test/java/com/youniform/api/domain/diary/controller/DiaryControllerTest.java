9081ab4b01aa31796a87c5119d4ba2f0a18527b2
정지연 <jung_j_yeon@naver.com>
2024-08-05 12:09:08 +0900

    feat(S11P12A308-662-be-test-container-inactivated): TestContainer 비활성화

--- backend/youniform/src/test/java/com/youniform/api/domain/diary/controller/DiaryControllerTest.java
+++ backend/youniform/src/test/java/com/youniform/api/domain/diary/controller/DiaryControllerTest.java
@@ -4,7 +4,6 @@ import com.epages.restdocs.apispec.ResourceSnippetParameters;
 import com.epages.restdocs.apispec.Schema;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.youniform.api.config.RedisTestContainerConfig;
 import com.youniform.api.domain.diary.dto.*;
 import com.youniform.api.domain.diary.dto.resource.*;
 import com.youniform.api.domain.diary.entity.Scope;
@@ -62,7 +61,6 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @AutoConfigureMockMvc
 @AutoConfigureRestDocs
 @ExtendWith(RestDocumentationExtension.class)
-@ExtendWith(RedisTestContainerConfig.class)
 @DisplayName("다이어리 API 명세서")
 @WithMockUser
 public class DiaryControllerTest {
