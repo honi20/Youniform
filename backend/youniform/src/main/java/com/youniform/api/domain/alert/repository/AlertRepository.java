03cf29e6b286d4bd2ce6d896022609029812a5e2
최예헌 <1594cyh@gmail.com>
2024-08-06 10:42:21 +0900

    feat(S11P12A308-664-be-alert-list): alert list

--- backend/youniform/src/main/java/com/youniform/api/domain/alert/repository/AlertRepository.java
+++ backend/youniform/src/main/java/com/youniform/api/domain/alert/repository/AlertRepository.java
@@ -2,6 +2,10 @@ package com.youniform.api.domain.alert.repository;
 
 import com.youniform.api.domain.alert.entity.Alert;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface AlertRepository extends JpaRepository<Alert, Long> {
+	List<Alert> findByReceiverIdAndIsDeletedFalse(@Param("userId") Long userId);
 }
