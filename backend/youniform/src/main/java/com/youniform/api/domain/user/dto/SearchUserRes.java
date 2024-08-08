0ff0abc9d4991c22891c348f9bc3ad1c98dd2edc
정지연 <jung_j_yeon@naver.com>
2024-08-08 09:57:57 +0900

    test(S11P12A308-732-be-friends-list): 유저 관련 테스트 작성

--- backend/youniform/src/main/java/com/youniform/api/domain/user/dto/SearchUserRes.java
+++ backend/youniform/src/main/java/com/youniform/api/domain/user/dto/SearchUserRes.java
@@ -1,11 +1,15 @@
 package com.youniform.api.domain.user.dto;
 
 import com.youniform.api.global.dto.SliceDto;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class SearchUserRes {
     private SliceDto<SearchUserDto> userList;
 
