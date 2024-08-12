a8b718f8c9ad5e9fd743d73b94b2cf47cfe98ae5
고민혁 <rhalsgur0086@gmail.com>
2024-08-12 10:28:18 +0900

    feat(S11P12A308-777-be-security-filter): security filter 경로 수정

--- backend/youniform/src/main/java/com/youniform/api/global/config/SecurityConfig.java
+++ backend/youniform/src/main/java/com/youniform/api/global/config/SecurityConfig.java
@@ -41,12 +41,12 @@ public class SecurityConfig {
                 .formLogin(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
                                 .requestMatchers(CorsUtils::isPreFlightRequest).permitAll()
-                                .requestMatchers("/admin/**").hasRole("ADMIN")
-                                .requestMatchers("/users/*/check-duplication",
-                                        "/users/signup/**",
-                                        "/users/find/password", "/users/check/email",
+                                .requestMatchers(
+                                        "/users/signup/**", "/users/signin/local",
+                                        "/users/verify", "/users/email/send",
+                                        "/users/email/verify", "/users/check/email",
                                         "/users/password/reset", "/users/password/send",
-                                        "/docs/**", "/swagger-ui/**", "/v3/api-docs/**", "/h2-console/**").permitAll()
+                                        "/docs/**", "/swagger-ui/**", "/v3-docs/**", "/h2-console/**").permitAll()
 //                        .requestMatchers("/**").permitAll()
                                 .anyRequest().authenticated()
                 )
