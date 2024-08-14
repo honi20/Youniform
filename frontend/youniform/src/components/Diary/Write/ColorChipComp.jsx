c3e1ffb1eeef2e522064b7c8b23c1c93e7bb699f
박주연 <ekscn09@naver.com>
2024-08-14 14:59:10 +0900

    feat(S11P12A308-880-fe-connection-of-resource-krozv): diary resource connection

--- frontend/youniform/src/components/Diary/Write/ColorChipComp.jsx
+++ frontend/youniform/src/components/Diary/Write/ColorChipComp.jsx
@@ -1,13 +1,10 @@
 import React from "react";
 import styled from "styled-components";
-
+import useResourceStore from "@stores/resoureStore";
 const Div = styled.div`
   box-sizing: border-box;
   height: 40px;
   width: 100%;
-
-  /* background-color: purple; */
-  /* border: 1px solid purple; */
   display: flex;
   justify-content: center;
   align-items: end;
@@ -16,26 +13,41 @@ const Colorchip = styled.div`
   width: 28px;
   height: 28px;
   margin: 0 4px;
-  border: 1px solid black;
+  border: 1px solid;
   border-radius: 10px;
   background-color: ${(props) => (props.$color ? props.$color : "black")};
+  border-color: ${(props) =>
+    props.$isClicked ? props.theme.primary : "white"};
 `;
+
 const colors = [
-  { index: 0, color: "#FFFFFF" },
-  { index: 1, color: "#EBEBEB" },
-  { index: 2, color: "#FFDCDC" },
-  { index: 3, color: "#FFD3B2" },
-  { index: 4, color: "#FFF7C0" },
-  { index: 5, color: "#C8FFC4" },
-  { index: 6, color: "#C1E3FF" },
-  { index: 7, color: "#E8C9FF" },
-  { index: 8, color: "#000000" },
+  { index: 0, name: "WHITE", color: "#FFFFFF" },
+  { index: 1, name: "GRAY", color: "#EBEBEB" },
+  { index: 2, name: "RED", color: "#FFDCDC" },
+  { index: 3, name: "ORANGE", color: "#FFD3B2" },
+  { index: 4, name: "YELLOW", color: "#FFF7C0" },
+  { index: 5, name: "GREEN", color: "#C8FFC4" },
+  { index: 6, name: "BLUE", color: "#C1E3FF" },
+  { index: 7, name: "PURPLE", color: "#E8C9FF" },
+  { index: 8, name: "BLACK", color: "#000000" },
 ];
 const ColorChipComp = () => {
+  const { setSelectedColor, selectedColor } = useResourceStore((state) => ({
+    setSelectedColor: state.setSelectedColor,
+    selectedColor: state.selectedColor,
+  }));
   return (
     <Div>
+      {/* {selectedColor} */}
       {colors.map((elem, index) => {
-        return <Colorchip key={index} $color={elem.color} />;
+        return (
+          <Colorchip
+            key={index}
+            $color={elem.color}
+            onClick={() => setSelectedColor(elem.name)}
+            $isClicked={elem.name == selectedColor}
+          />
+        );
       })}
     </Div>
   );
